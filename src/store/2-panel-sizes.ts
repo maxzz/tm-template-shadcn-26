10753e39896b29912da091d2cf87b142e220d436
Max Zakharzhevskiy <maxzz@msn.com>
2026-06-04 20:33:45 -0700

    WIP. PanelSizes code review. 2

--- src/store/2-panel-sizes.ts
+++ src/store/2-panel-sizes.ts
@@ -9,43 +9,10 @@ export function getValidPanelSizes(parsedSizes?: unknown): PanelSizes {
     const defaultHorizontal = { left: 30, right: 70 };
     const defaultVertical = { top: 50, bottom: 50 };
 
-    if (!parsedSizes) {
-        return {
-            horizontal: defaultHorizontal,
-            vertical: defaultVertical,
-        };
-    }
-
-    let horizontal: Layout = defaultHorizontal;
-    let vertical: Layout = defaultVertical;
-
-    if (parsedSizes !== null) {
-        const obj = parsedSizes as Record<string, any>;
-
-        // Validate horizontal
-        if (obj.horizontal && typeof obj.horizontal === 'object') {
-            const h = obj.horizontal as Record<string, any>;
-            horizontal = {
-                left: typeof h.left === 'number' ? h.left : defaultHorizontal.left,
-                right: typeof h.right === 'number' ? h.right : defaultHorizontal.right,
-            };
-        }
-
-        // Validate vertical
-        if (obj.vertical && typeof obj.vertical === 'object') {
-            const v = obj.vertical as Record<string, any>;
-            vertical = {
-                top: typeof v.top === 'number' ? v.top : defaultVertical.top,
-                bottom: typeof v.bottom === 'number' ? v.bottom : defaultVertical.bottom,
-            };
-        }
-    } else {
-        horizontal = defaultHorizontal;
-        vertical = defaultVertical;
-    }
-
-    return {
-        horizontal,
-        vertical,
+    const rv: PanelSizes = parsedSizes as PanelSizes ?? {
+        horizontal: defaultHorizontal,
+        vertical: defaultVertical,
     };
+
+    return rv;
 }
