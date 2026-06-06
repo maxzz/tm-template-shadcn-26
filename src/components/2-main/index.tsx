51e1c913efbd7664cb1b5cef6b08753e95c0af80
Max Zakharzhevskiy <maxzz@msn.com>
2026-06-05 21:46:17 -0700

    WIP. add new test dialog

--- src/components/2-main/index.tsx
+++ src/components/2-main/index.tsx
@@ -1,11 +1,14 @@
-import { TestConfirmationDialog, TestResizablePanels } from "./xyz-demos";
+import { TestConfirmationDialog, TestResizablePanels, TestLoginDialog } from "./xyz-demos";
 
 export function MainBody() {
     return (
         <main className="px-2 py-3 flex flex-col gap-2">
             <TestResizablePanels className="w-full h-[400px] border rounded-xl bg-card text-card-foreground shadow-xs overflow-hidden" />
 
-            <TestConfirmationDialog />
+            <div className="flex gap-2">
+                <TestConfirmationDialog />
+                <TestLoginDialog />
+            </div>
         </main>
     );
 }
